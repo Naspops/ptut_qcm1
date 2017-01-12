e8a77f396b63872fac4aa32a5991f38db79edf6e
p1605917 <p1605917@IUTABGDIN105-12.univ-lyon1.fr>
2017-01-12 10:00:47 +0100

    Test

---
+++ src/ptut_qcm/Ptut_qcm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ptut_qcm;
+
+/**
+ *
+ * @author p1605917
+ */
+public class Ptut_qcm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+    }
+    
+}
